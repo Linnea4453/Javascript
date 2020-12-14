83b491fae13cd806417ffc29d74bebb03746c746
Linnea Nilsson <linnea.nilsson23@learnet.se>
2020-12-14 13:41:06 +0100

    Lektion1

---
+++ lektion-1/1. comments/script.js
@@ -0,0 +1,12 @@
+// inline comment crtl+'
+var user = {}; // Det här skapar en användare
+
+/* multi line comment 
+shift+alt+a */
+
+
+/* Hur mycket ska jag kommentera i min JS-kod
+ svar: lagom
+
+    var user = {};
+*/
---
+++ lektion-1/2. console/script.js
@@ -0,0 +1,32 @@
+/* 
+    CONSOLE
+    Används för felsökning. Är typ den enda felsökningsmetoden vi har
+    att använda oss utav.
+
+    Ska bara användas i utvecklingsmiljön. Inte i produktionsmiljön.
+    Dock finns vissa undantag.
+
+    Det finns andra typer av felsökningsverktyg som heter ESLint.
+
+    I Webbläsaren: Granska/Inspektera -> Console
+
+
+    Sätt att felsöka på:
+    myfunc()
+
+    function myfunc() {
+        console.logg('1')
+
+        if(false) {
+            console.log('2')
+
+        } else {
+            console.log('3')
+        }
+    }
+
+*/
+console.log(".log() = informationstext")
+console.warn('.warn = varningsmeddelande')
+console.error('.error = felmeddelande')
+
